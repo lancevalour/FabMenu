9455ca22407854b36e7e283df7540b306981ca11
ZhangY <zhangyicheng1234@gmail.com>
2015-12-22 11:02:52 -0500

    Finish addFab() method

--- app/src/main/java/com/zyceeit/android/fabmenu/sample/MainActivity.java
+++ app/src/main/java/com/zyceeit/android/fabmenu/sample/MainActivity.java
@@ -2,13 +2,19 @@ package com.zyceeit.android.fabmenu.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+import android.widget.Toast;
 
 import com.zyceeit.android.fabmenu.FabMenu;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ZhangY on 12/18/2015.
  */
-public class MainActivity extends Activity{
+public class MainActivity extends Activity {
 
     FabMenu fabMenu;
 
@@ -18,19 +24,59 @@ public class MainActivity extends Activity{
         setContentView(R.layout.activity_main);
 
         initiateComponents();
-        setComponentControl();
+        //  setComponentControl();
 
     }
 
-    private void initiateComponents(){
+    private void initiateComponents() {
         fabMenu = (FabMenu) findViewById(R.id.fabMenu);
+        fabMenu.addFab();
+        fabMenu.addFab();
+        fabMenu.addFab();
+        fabMenu.addFab();
+        fabMenu.addFab();
 
     }
 
-    private void setComponentControl(){
+    private void setComponentControl() {
+        List<View.OnClickListener> onClickListenerList = new ArrayList<>();
 
+        onClickListenerList.add(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "sub 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        onClickListenerList.add(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "sub 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        onClickListenerList.add(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "sub 3", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+   /*     onClickListenerList.add(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "sub 4", Toast.LENGTH_SHORT).show();
+            }
+        });
+        onClickListenerList.add(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "sub 5", Toast.LENGTH_SHORT).show();
+            }
+        });
+*/
+        fabMenu.setSubFabOnClickListener(onClickListenerList);
     }
-
 
 
 }
--- fabmenu/src/main/java/com/zyceeit/android/fabmenu/FabMenu.java
+++ fabmenu/src/main/java/com/zyceeit/android/fabmenu/FabMenu.java
@@ -1,9 +1,12 @@
 package com.zyceeit.android.fabmenu;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.support.design.widget.FloatingActionButton;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -17,20 +20,33 @@ import java.util.List;
  */
 public class FabMenu extends RelativeLayout {
 
-  /*  List<FloatingActionButton> fabList;*/
+    RelativeLayout fab_layout;
+
+    List<FloatingActionButton> subFabList;
+
+    boolean isSubFabVisible = false;
 
     Context context;
 
-    // private Button button;
     private FloatingActionButton fab;
-    private FloatingActionButton fab_sub1, fab_sub2, fab_sub3;
+    //  private FloatingActionButton fab_sub1, fab_sub2, fab_sub3;
+
+    int fabSizeInDP = 56;
+    int subFabSizeInDP = 46;
+    int radiusInDP = 80;
+    int fabMarginInDP = 20;
+
+    int fabSize;
+    int subFabSize;
+    int radius;
+    int fabMargin;
 
 
     public FabMenu(Context context) {
         super(context);
         this.context = context;
         initiateViews();
-        //fabList = new ArrayList<>();
+        subFabList = new ArrayList<>();
     }
 
 
@@ -38,12 +54,14 @@ public class FabMenu extends RelativeLayout {
         super(context, attrs);
         this.context = context;
         initiateViews();
+        subFabList = new ArrayList<>();
     }
 
     public FabMenu(Context context, AttributeSet attrs, int defStyle) {
         this(context, attrs);
         this.context = context;
         initiateViews();
+        subFabList = new ArrayList<>();
     }
 
     private void initiateViews() {
@@ -55,25 +73,158 @@ public class FabMenu extends RelativeLayout {
     protected void onFinishInflate() {
         super.onFinishInflate();
 
+        fab_layout = (RelativeLayout) findViewById(R.id.fab_layout);
+        System.out.println("num of views " + fab_layout.getChildCount());
+
         fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        //textView = (TextView) findViewById(R.id.textView);
+        subFabSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, subFabSizeInDP, getResources().getDisplayMetrics());
+
+        radius = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, radiusInDP, getResources().getDisplayMetrics());
+
+        fabMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, fabMarginInDP, getResources().getDisplayMetrics());
+
+        fabSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, fabSizeInDP, getResources().getDisplayMetrics());
+
+
+        LayoutParams params = new LayoutParams(
+                fabSize,
+                fabSize
+        );
+
+        params.setMargins(0, 0, fabMargin, fabMargin);
+        params.addRule(RelativeLayout.ALIGN_PARENT_END);
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+
+        fab.setLayoutParams(params);
+
+
+        setFabMenuControl();
+
+        closeMenu();
+    }
+
+    public void openMenu() {
+        isSubFabVisible = true;
+        setSubFabVisible(isSubFabVisible);
+    }
+
+    public void closeMenu() {
+        isSubFabVisible = false;
+        setSubFabVisible(isSubFabVisible);
+    }
+
+    private void setSubFabVisible(boolean isSubFabVisible) {
+        if (isSubFabVisible) {
+            for (FloatingActionButton subFab : subFabList) {
+                subFab.show();
+            }
+        } else {
+            for (FloatingActionButton subFab : subFabList) {
+                subFab.hide();
+            }
+        }
+    }
+
+    private void setFabMenuControl() {
+        fab.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isSubFabVisible) {
+                    closeMenu();
+                } else {
+                    openMenu();
+                }
+            }
+        });
+    }
+
+    public void setSubFabOnClickListener(List<OnClickListener> onClickListenerList) {
+       /* for (int i = 0; i < onClickListenerList.size(); i++) {
+            subFabList.get(i).setOnClickListener(onClickListenerList.get(i));
+        }*/
     }
 
 
+    public void addFab() {
+        //this.subFabList.add(fab);
+        //int numOfSubFabs = subFabList.size();
+        int numOfSubFabs = this.fab_layout.getChildCount() - 1;
 
-/*public void addFab(FloatingActionButton fab) {
-        this.fabList.add(fab);
+        if (numOfSubFabs == 0) {
+            LayoutParams params = new LayoutParams(
+                    subFabSize,
+                    subFabSize
+            );
+
+            params.setMargins(0, 0, radius + fabMargin, fabMargin);
+            params.addRule(RelativeLayout.ALIGN_PARENT_END);
+            params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+
+            FloatingActionButton subFab = new FloatingActionButton(getContext());
+            subFab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardview_light_background)));
+            this.fab_layout.addView(subFab, 1, params);
+            this.subFabList.add(0, subFab);
+
+        } else if (numOfSubFabs == 1) {
+            LayoutParams params = new LayoutParams(
+                    subFabSize,
+                    subFabSize
+            );
+
+            params.setMargins(0, 0, fabMargin, radius + fabMargin);
+            params.addRule(RelativeLayout.ALIGN_PARENT_END);
+            params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+
+            FloatingActionButton subFab = new FloatingActionButton(getContext());
+            subFab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardview_light_background)));
+            this.fab_layout.addView(subFab, 2, params);
+            this.subFabList.add(1, subFab);
+
+        } else {
+            float degInterval = 90 / numOfSubFabs;
+
+            for (int i = 0; i < numOfSubFabs - 2; i++) {
+                this.fab_layout.removeViewAt(-i + numOfSubFabs);
+                this.subFabList.remove(-i + numOfSubFabs- 1);
+            }
+
+            for (int i = 0; i < numOfSubFabs - 1; i++) {
+                int marginX = (int) (radius * Math.cos(Math.toRadians(degInterval * (i + 1))));
+                int marginY = (int) (radius * Math.sin(Math.toRadians(degInterval * (i + 1))));
+
+                System.out.println(marginX);
+                System.out.println(marginY);
+
+                LayoutParams params = new LayoutParams(
+                        subFabSize,
+                        subFabSize
+                );
+
+                params.setMargins(0, 0, marginX + fabMargin, marginY + fabMargin);
+                params.addRule(RelativeLayout.ALIGN_PARENT_END);
+                params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+
+                FloatingActionButton subFab = new FloatingActionButton(getContext());
+                subFab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardview_dark_background)));
+                this.fab_layout.addView(subFab, i + 3, params);
+                this.subFabList.add(i + 2, subFab);
+            }
+        }
+
+  /*      params.setMargins(0, 0, (-numOfSubFabs + 1) * 30, (numOfSubFabs ) * 80 + 50);
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        params.addRule(RelativeLayout.START_OF, R.id.fab);*/
+
     }
 
     public void removeFab(FloatingActionButton fab) {
-        this.fabList.remove(fab);
+        this.subFabList.remove(fab);
     }
 
     public void removeFab(int index) {
-        this.fabList.remove(index);
+        this.subFabList.remove(index);
     }
-*/
 
 
 }
